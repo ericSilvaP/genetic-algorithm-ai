2858dc1422b9d8b9d1605947d62c0150c77d9435
Eric Silva <ericsilvap13@gmail.com>
2026-04-07 11:34:30 -0300

    feat: fitness functions aluffi and camel back 3

---
+++ fitness_functions.py
@@ -0,0 +1,8 @@
+def aluffi_pentini(ind):
+    x, y = ind
+    return 0.25 * x**4 - 0.5 * x**2 + 0.1 * x + 0.5 * y**2
+
+
+def three_camel_back(ind):
+    x, y = ind
+    return 2 * x**2 - 1.05 * x**4 + (x**6) / 6 + x * y + y**2
