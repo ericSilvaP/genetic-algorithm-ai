c647334b5a2cad196983c3316347dae33a209095
Eric Silva <ericsilvap13@gmail.com>
2026-04-07 17:10:34 -0300

    feat: separate two problems situations

--- config_type.py
+++ config_type.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Callable
 
+from fitness_functions import aluffi_pentini, three_camel_back
+
 
 @dataclass
 class FunctionConfig:
@@ -8,3 +10,9 @@ class FunctionConfig:
     bounds: tuple
     optimum: float
     n: int
+
+
+FUNCTIONS_CONFIG = {
+    "AP": FunctionConfig(func=aluffi_pentini, bounds=(-10, 10), optimum=-0.3523, n=2),
+    "CB3": FunctionConfig(func=three_camel_back, bounds=(-5, 5), optimum=0.0, n=2),
+}
--- genetic_alg.py
+++ genetic_alg.py
@@ -1,23 +1,16 @@
 from create_population import create_population
-from fitness_functions import aluffi_pentini, format_decimals, three_camel_back
-from config_type import FunctionConfig
+from config_type import FUNCTIONS_CONFIG, FunctionConfig
 from selection import crossover, mutate, selection
 
-# =========================
-# CONFIGURAÇÕES
-# =========================
-POP_SIZE = 50
-GENERATIONS = 200
-MUTATION_RATE = 0.3
-FUNCTIONS_CONFIG = {
-    "AP": FunctionConfig(func=aluffi_pentini, bounds=(-10, 10), optimum=-0.3523, n=2),
-    "CB3": FunctionConfig(func=three_camel_back, bounds=(-5, 5), optimum=0.0, n=2),
-}
 NFE = 0
 
 
 def genetic_algorithm(
-    pop_size, generations, function_config: FunctionConfig, mutation_rate
+    pop_size,
+    generations,
+    function_config: FunctionConfig,
+    mutation_rate=None,
+    best_fit_stop_count=10,
 ):
     population = create_population(pop_size, function_config.bounds, function_config.n)
     fitness = function_config.func
@@ -35,16 +28,18 @@ def genetic_algorithm(
         else:
             best_continous_count += 1
 
-        if best_continous_count >= 10:
+        if best_continous_count >= best_fit_stop_count:
             break
 
+        # Gerar nova população
         new_population = []
         for _ in range(pop_size):
             p1 = selection(population, fitness)
             p2 = selection(population, fitness)
 
             child = crossover(p1, p2)
-            child = mutate(child, mutation_rate, function_config.bounds)
+            if mutation_rate:
+                child = mutate(child, mutation_rate, function_config.bounds)
 
             new_population.append(child)
 
@@ -53,12 +48,20 @@ def genetic_algorithm(
     return best_fitness
 
 
-def success_rate(function_config: FunctionConfig, runs=100):
+def success_rate(
+    function_config: FunctionConfig,
+    pop_size,
+    generations,
+    best_fit_stop_count,
+    runs=100,
+    mutation_rate=None,
+):
     successes = 0
 
     for _ in range(runs):
-        value = genetic_algorithm(POP_SIZE, GENERATIONS, function_config, MUTATION_RATE)
-        print(value)
+        value = genetic_algorithm(
+            pop_size, generations, function_config, mutation_rate, best_fit_stop_count
+        )
 
         if abs(value - function_config.optimum) < 0.01:
             successes += 1
@@ -72,9 +75,37 @@ def evaluate(ind, fitness):
     return fitness(ind)
 
 
-# =========================
-# EXECUÇÃO
-# =========================
-sr = success_rate(FUNCTIONS_CONFIG["AP"])
+# Aluffi
+POP_SIZE = 50
+GENERATIONS = 200
+MUTATION_RATE = 0.3
+BEST_FIT_STOP_COUNT = 25
+
+runs = 100
+sr = success_rate(
+    FUNCTIONS_CONFIG["AP"],
+    POP_SIZE,
+    GENERATIONS,
+    BEST_FIT_STOP_COUNT,
+    runs=runs,
+    mutation_rate=MUTATION_RATE,
+)
 print(f"Taxa de sucesso AP: {sr}%")
-print("NFE:", NFE)
+print("NFE:", round(NFE / runs))
+
+# Camel Back 3
+POP_SIZE = 50
+GENERATIONS = 200
+BEST_FIT_STOP_COUNT = 5
+NFE = 0
+
+runs = 100
+sr = success_rate(
+    FUNCTIONS_CONFIG["CB3"],
+    POP_SIZE,
+    GENERATIONS,
+    BEST_FIT_STOP_COUNT,
+    runs=runs,
+)
+print(f"Taxa de sucesso CB3: {sr}%")
+print("NFE:", round(NFE / runs))
